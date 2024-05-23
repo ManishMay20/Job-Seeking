1690bb692d447c6682d4c35ec1c81929380757b1
ManishMay20 <manishpatidar73289@gmail.com>
2024-05-23 12:01:49 +0530

    rename server.js to index.js

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,10 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+
+app.get("/", (req, res, next) => {
+  res.send("hello");
+});
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
